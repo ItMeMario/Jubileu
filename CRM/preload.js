fb16a7b197e6c5220942d080d1244d1d07076084
ItMeMario <47013194+ItMeMario@users.noreply.github.com>
2026-01-30 13:33:54 -0300

    remoção do CRM

--- CRM/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  connectWhatsapp: () => ipcRenderer.invoke('connect-whatsapp'),
-  onQrCode: (callback) => ipcRenderer.on('qr-code', (event, qr) => callback(qr)),
-  onConnected: (callback) => ipcRenderer.on('connected', () => callback()),
-  onLog: (callback) => ipcRenderer.on('log', (event, msg) => callback(msg))
-});
